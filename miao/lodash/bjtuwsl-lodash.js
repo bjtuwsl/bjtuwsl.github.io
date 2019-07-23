5ef94ffa8b45783e424545889ee3bfc62d7c4f84
bjtuwsl <1783728365@qq.com>
2019-07-23 19:45:16 +0800

    lodash 1st

--- miao/lodash/bjtuwsl-lodash.js
+++ miao/lodash/bjtuwsl-lodash.js
@@ -1,5 +1,68 @@
 var bjtuwsl = {
+  /**
+   * 
+   * @param {array} ary 
+   * @return {array}
+   */
   compact: function (ary) {
-    return ary.filter(it => it)
-  }
+    return ary.filter(it => it);
+  },
+
+  /**
+   * 
+   * @param {array} ary 
+   * @param {number} size 
+   * @return {array}
+   */
+  chunk: function (ary, size = 1) {
+    var res = [];
+    for(var i = 0; i < ary.length; i += size){
+      res.push(ary.slice(i, i + size));
+    }
+    return res;
+  },
+  /**
+   * 
+   * @param {array} ary 
+   * @param  {...args} args 
+   * @return {array}
+   */
+  difference: function(ary, ...args){
+    var arr = args.reduce((res, cur) => {
+      return res.concat(cur);
+    })
+    return ary.filter(item => !arr.includes(item));
+  },
+  /**
+   * 
+   * @param {array} ary 
+   * @param {number} num 
+   * @return {array}
+   */
+  drop: function (ary, num = 1){
+    return ary.slice(num);
+  },
+  /**
+   * 
+   * @param {array} arr 
+   * @param {number} num 
+   * @return {array}
+   */
+  dropRight:function(ary, num = 1){
+    if(num >= ary.length){
+      return [];
+    }
+    return ary.slice(0, ary.length - num);
+  },
+  /**
+   * 
+   * @param {function} f
+   * @return {function}
+   */
+  negate: function (f) {
+    return function (...arg){
+      return !f(...arg);
+    }
+  },
 }
+
